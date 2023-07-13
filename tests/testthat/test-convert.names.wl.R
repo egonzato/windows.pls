9aa2ec830c01ea086f9663c92b88b437e309a893
Elia Gonzato <elia.gonzato@outlook.it>
2023-07-13 15:42:10 +0200

    Added test for 'convert.names.wl'

---
+++ tests/testthat/test-convert.names.wl.R
@@ -0,0 +1,17 @@
+library(testthat);
+context("Converting names to valid syntax testing");
+source("convert.names.wl.R");
+
+
+data("beer")
+conc=unlist(beer[,1])
+spectra=beer[,2:ncol(beer)]
+names(spectra)=convert.names.wl(1100,2250,2)
+
+#
+test_that(desc="The best window is Window1",code ={
+  name4=names(spectra)[4];
+  expect_that(object=name4,condition=equals('X1106'));
+  expect_that(object = is.character(name4),condition = equals(TRUE))
+
+} )
